eac3e230b33e8ec4b6b2fefd36f9758660d6ada6
bandarupavankumar <bandarupavankumar555@gmail.com>
2021-09-24 17:47:51 +0530

    okie

--- javacomplete/assignments/fifteen.java
+++ javacomplete/assignments/fifteen.java
@@ -1,9 +1,10 @@
 //14.write a java program to print 15 to 10 number using for while/while do while//
 class fifteen{
     public static void main(String[] k){
-        for(int i=10;i>=0;i=i-1){
+        int i=0;
+        while(i<=10){
             System.out.println(i);
-            i=i+2;
+            i=i+2;        
         }
     }
 }
